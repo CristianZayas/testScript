a82416aa8ba57d9710bbfe8ec3199e0df76587b1
Cristian Zayas <67510541+CristianZayas@users.noreply.github.com>
2025-07-10 16:57:53 -0600

    Update main.js

--- test/main.js
+++ test/main.js
@@ -1,4 +1,6 @@
 import { z } from 'z/v4';
 export const UserSchema = z.object({
-    name: z.string().trim()
+    name: z.string().trim(),
+    lastname: z.string().trim(),
+    email: z.email()
 }) 
